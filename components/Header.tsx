5194026a0bfe6479fd43c872724f7c0f1f6b7dff
Julie <jleskovets@gmail.com>
2026-09-13 13:12:41 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,9 +17,10 @@ export default function Header() {
             href="/meal-flow-family"
             className="hover:text-teal-700"
           >
+          {/*
             Meal Flow Family
           </a>
-          
+           */}
           <a
             href="/pro-analytics"
             className="hover:text-teal-700"
